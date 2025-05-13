bd24308672bdd2112c126ad2bb82728e2a21f02b
mahatabul <rozinislam2002@gmail.com>
2025-05-13 17:40:29 +0600

    created book and member class

--- main.cpp
+++ main.cpp
@@ -3,26 +3,76 @@
 using namespace std;
 using ll = long long;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
 
+class Member
+{
+private:
+    string name;
+    string memID;
+    string password;
 
-class publication{
-    protected:
-    string author,publication; int edition;
-    public:
+public:
+    Member(string n, string m, string p)
+        : name(n), memID(m), password(p) {}
 
-
+    void mem_details()
+    {
+        cout << "\Member Details\n";
+        cout << this->name << endl;
+        cout << this->memID << endl;
+    }
+    void setmem_details(string n, string id, string p)
+    {
+        this->name = n;
+        this->memID = id;
+        this->password = p;
+    }
 };
-class Library{
+class Book
+{
+protected:
+    string title;
+    bool avail;
+    string author;
+    string publisher;
+    int edition;
 
-    private:
+public:
+    Book(string t, string auth, string pub, int e, bool av)
+        : title(t), author(auth), publisher(pub), edition(e), avail(av) {}
 
+    void book_details()
+    {
+        cout << "\nBook Details\n";
+        cout << this->title << endl;
+        cout << this->author << endl;
+        cout << this->publisher << endl;
+        cout << this->edition << endl;
+    }
+    void setbook_details(string t, string auth, string pub, int e, bool av)
+    {
 
+        this->title = t;
+        this->author = auth;
+        this->publisher = pub;
+        this->edition = e;
+        this->avail = av;
+    }
 };
-int main() {
+class Library
+{
+
+private:
+    vector<Book> books;
+    vector<Member> members;
+};
+int main()
+{
     fastio;
-
-    
 
     return 0;
 }
