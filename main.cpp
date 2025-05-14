68d445303ea70c0e84154d3ffe7aebf23654d7d8
mahatabul <rozinislam2002@gmail.com>
2025-05-14 09:48:07 +0600

    added addmember and addbook func

--- main.cpp
+++ main.cpp
@@ -8,30 +8,6 @@ using ll = long long;
     cin.tie(0);                       \
     cout.tie(0);
 
-class Member
-{
-private:
-    string name;
-    string memID;
-    string password;
-
-public:
-    Member(string n, string m, string p)
-        : name(n), memID(m), password(p) {}
-
-    void mem_details()
-    {
-        cout << "\nMember Details\n";
-        cout << this->name << endl;
-        cout << this->memID << endl;
-    }
-    void setmem_details(string n, string id, string p)
-    {
-        this->name = n;
-        this->memID = id;
-        this->password = p;
-    }
-};
 class Book
 {
 protected:
@@ -68,6 +44,36 @@ public:
     string get_publisher() { return publisher; }
     int get_edition() { return edition; }
 };
+
+class Member
+{
+private:
+    string name;
+    string memID;
+    string password;
+    vector<Book> borrowed;
+
+public:
+    Member(string n, string m, string p)
+        : name(n), memID(m), password(p) {}
+
+    void mem_details()
+    {
+        cout << "\nMember Details\n";
+        cout << this->name << endl;
+        cout << this->memID << endl;
+    }
+    void setmem_details(string n, string id, string p)
+    {
+        this->name = n;
+        this->memID = id;
+        this->password = p;
+    }
+    string get_name() { return name; }
+    string get_memID() { return memID; }
+};
+
+
 class Library
 {
 
@@ -75,8 +81,16 @@ private:
     vector<Book> books;
     vector<Member> members;
 
-    void addBook(Book b) { books.push_back(b); }
-    void addMember(Member m) { members.push_back(m); }
+    void addBook(string title = "", string author = "", string pub = "", int e = 0, bool aval = true)
+    {
+        Book b(title, author, pub, e, aval);
+        books.push_back(b);
+    }
+    void addMember(string n = "", string id = "", string pass = "")
+    {
+        Member m(n, id, pass);
+        members.push_back(m);
+    }
     void book_list()
     {
         int i = 1;
@@ -88,12 +102,36 @@ private:
     }
 
 public:
-    void add_book(string title, string author = "Unknown", string pub = "Unknown", int e = 0000, bool avail = true)
+    void add_book()
     {
-        Book b(title, author, pub, e, avail);
-        addBook(b);
+
+        string title, author, pub;
+        int e;
+        cout << "Adding new book....\n";
+        cout << "Set title: ";
+        cin >> title;
+        cout << "\nSet Author: ";
+        cin >> author;
+        cout << "\nSet Publisher: ";
+        cin >> pub;
+        cout << "\nSet Editon: ";
+        cin >> e;
+
+        addBook(title, author, pub, e, true);
     }
-    void add_member(Member m) { addMember(m); }
+    void add_member()
+    {
+        string name, memid, password;
+        cout << "Adding new member....\n";
+        cout << "Set Name: ";
+        cin >> name;
+        cout << "\nSet memID: ";
+        cin >> memid;
+        cout << "\nSet Password: ";
+        cin >> password;
+
+        addMember(name, memid, password);
+    }
     void show_book_list()
     {
         book_list();
@@ -110,7 +148,6 @@ public:
 };
 int main()
 {
-    
 
     return 0;
 }
